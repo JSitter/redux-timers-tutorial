3a6e9557bd470003fe422131473593839f63bee0
Justin Sitter <justin@jaytria.com>
2019-06-01 16:42:15 -0700

    Refactor to Timer component

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,8 +1,20 @@
-class Timer{
-  constructor(name){
-    this.name = name;
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addTimer } from '../actions/index.js';
+
+
+
+class Timer extends Component {
+  constructor(props){
+    super(props);
+    this.name = this.props.name;
     this.time = 0;
     this.isRunning = false;
+  }
+  render(){
+    return (
+      <div>timer {this.props.name}</div>
+    )
   }
 }
 
